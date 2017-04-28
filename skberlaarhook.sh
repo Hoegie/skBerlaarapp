b612b6a170bb593d4fed7ed4c354498dbf2909c2
Sven De Gronckel <Sven@Svens-MacBook-Pro.local>
2017-04-28 10:37:39 +0200

    3th commit

---
+++ skberlaarhook.sh
@@ -0,0 +1,3 @@
+date >> /volume1/web/nodejs/gitHub/logs/skberlaarhook.log 2>&1
+cd /volume1/web/nodejs/gitHub/skBerlaarapp
+git pull https://github.com/Hoegie/skBerlaarapp.git >> /volume1/web/nodejs/gitHub/logs/skberlaarhook.log 2>&1
